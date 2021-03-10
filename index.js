6dec4eb21d9ec7bffc93852788648f984ff88b16
SolomonKitonyi <kitonyisolomon@gmail.com>
2021-03-10 17:04:37 +0300

    add changes

--- index.js
+++ index.js
@@ -19,20 +19,35 @@ app.get('/api/courses/:id',(req,res)=>{
     res.send(course);
 });
 app.post('/api/courses',(req,res)=>{
-    const schema = Joi.object({
-        name:Joi.string().min(3).required()
-    })
-    const result = schema.validate(req.body)
-    if(result.error){
-        res.status(400).send(result.error.details[0].message);
-        return;
-    }
+    const {error} = validateCourse(req.body);
+    if(error)return res.status(400).send(error.details[0].message);
     const course = {
         id:courses.length + 1,
         name: req.body.name
     }
     courses.push(course);
     res.send(course);
+});
+app.put('/api/courses/:id',(req,res)=>{
+    const course = courses.find(c=> c.id === parseInt(req.params.id));
+    if(!course) return res.status(404).send("The course you are trying to search was not found");
+    const {error} = validateCourse(req.body);
+    if(error)return res.status(400).send(error.details[0].message);
+    course.name = req.body.name;
+    res.send(course);
+});
+app.delete('/api/courses/:id',(req,res)=>{
+    const course = courses.find(c=> c.id === parseInt(req.params.id));
+    if(!course) return res.status(404).send("The course you are trying to search was not found");
+    const index = courses.indexOf(course);
+    courses.splice(index,1);
+    res.send(course);
 })
+function validateCourse(course){
+    const schema = Joi.object({
+        name:Joi.string().min(3).required()
+    })
+    return schema.validate(course)
+}
 const port = process.env.PORT || 3000
 app.listen(port,()=>console.log(`Listening to port ${port}....`));
