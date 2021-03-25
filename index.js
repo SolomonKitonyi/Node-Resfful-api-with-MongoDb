79e704152b420cb13d70c3e6383bc51df2c6584b
SolomonKitonyi <kitonyisolomon@gmail.com>
2021-03-25 18:06:04 +0300

    add mongodb

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
+const mongoose = require('mongoose');
 const Joi = require('joi');
 const express = require('express');
 const app = express();
 const courses = require('./routes/courses');
 app.use(express.json());
 app.use('/api/courses',courses);
+
+mongoose.connect('mongodb://localhost/playground',{useNewUrlParser: true,useUnifiedTopology:true})
+ .then(()=>console.log("Connected To MongoDb....."))
+ .catch(err => console.log("Error",err));
 
 
 const port = process.env.PORT || 3000
--- routes/courses.js
+++ routes/courses.js
@@ -1,47 +1,60 @@
 const express = require('express');
 const Joi = require('joi');
+const  mongoose  = require('mongoose');
 const router = express.Router();
-const courses = [
-    {id:1,name:'course1'},
-    {id:2,name:'course2'},
-    {id:3,name:'course3'},
-];
+
+const Course = mongoose.model('Course',new mongoose.Schema({
+    name:{
+        type:String,
+        required: true,
+        minlength:4,
+        maxlength:25
+    }
+}));
+
 // router.get('/',(req,res)=>{
 //     res.send('Hello World!!!!!');
 // });
-router.get('/',(req,res)=>{
+router.get('/',async(req,res)=>{
+    const courses = await Course.find().sort('name');
     res.send(courses);
 });
-router.get('/:id',(req,res)=>{
-    const course = courses.find(c=> c.id === parseInt(req.params.id));
+
+
+router.get('/:id',async (req,res)=>{
+     const course = await Course.findById(req.params.id)
     if(!course) res.status(404).send("The course you are trying to search was not found");
     res.send(course);
 });
-router.post('/',(req,res)=>{
+
+
+router.post('/',async(req,res)=>{
     const {error} = validateCourse(req.body);
     if(error)return res.status(400).send(error.details[0].message);
-    const course = {
-        id:courses.length + 1,
-        name: req.body.name
-    }
-    courses.push(course);
+    let course = new Course ({ name: req.body.name});
+    course = await course.save(course);
     res.send(course);
 });
-router.put('/:id',(req,res)=>{
-    const course = courses.find(c=> c.id === parseInt(req.params.id));
-    if(!course) return res.status(404).send("The course you are trying to search was not found");
+
+
+router.put('/:id',async(req,res)=>{
     const {error} = validateCourse(req.body);
     if(error)return res.status(400).send(error.details[0].message);
-    course.name = req.body.name;
+    const course = await Course.findByIdAndUpdate(req.params.id,{name:req.body.name},{
+       new:true
+    });
+    if(!course) return res.status(404).send("The course you are trying to search was not found");
+   
     res.send(course);
 });
-router.delete('/:id',(req,res)=>{
-    const course = courses.find(c=> c.id === parseInt(req.params.id));
+
+router.delete('/:id',async(req,res)=>{
+    const course = await Course.findByIdAndRemove(req.params.id);
     if(!course) return res.status(404).send("The course you are trying to search was not found");
-    const index = courses.indexOf(course);
-    courses.splice(index,1);
     res.send(course);
-})
+});
+
+
 function validateCourse(course){
     const schema = Joi.object({
         name:Joi.string().min(3).required()
