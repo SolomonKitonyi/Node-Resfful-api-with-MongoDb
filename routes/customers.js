9c6b91237e3dfe89fc4981cb89f1c58a6625d6a9
SolomonKitonyi <kitonyisolomon@gmail.com>
2021-04-19 14:30:59 +0300

    separate customer model

--- routes/customers.js
+++ routes/customers.js
@@ -1,27 +1,8 @@
+const {Customer,validate} = require('../models/customer');
 const express = require('express');
-const Joi = require('joi');
-const  mongoose  = require('mongoose');
 const router = express.Router();
 
-const Customer = mongoose.model('Customer',new mongoose.Schema({
-    name:{
-        type:String,
-        required: true,
-        minlength:4,
-        maxlength:25
-    },
-    isGold:{
-        type:Boolean,
-        default: false
-    },
-    phone:{
-        type:String,
-        required: true,
-        minlength:4,
-        maxlength:25
-    }
 
-}));
 
 // router.get('/',(req,res)=>{
 //     res.send('Hello World!!!!!');
@@ -40,7 +21,7 @@ router.get('/:id',async (req,res)=>{
 
 
 router.post('/',async(req,res)=>{
-    const {error} = validateCustomer(req.body);
+    const {error} = validate(req.body);
     if(error)return res.status(400).send(error.details[0].message);
     let customer = new Customer ({
          name: req.body.name,
@@ -52,7 +33,7 @@ router.post('/',async(req,res)=>{
 });
 
 router.put('/:id',async(req,res)=>{
-    const {error} = validateCustomer(req.body);
+    const {error} = validate(req.body);
     if(error)return res.status(400).send(error.details[0].message);
     const customer = await Customer.findByIdAndUpdate(req.params.id,{name:req.body.name,phone:req.body.phone,isGold:req.body.isGold},{
        new:true
@@ -68,14 +49,5 @@ router.delete('/:id',async(req,res)=>{
     res.send(customer);
 });
 
-
-function validateCustomer(customer){
-    const schema = Joi.object({
-        name:Joi.string().min(5).max(50).required(),
-        phone:Joi.string().min(5).max(50).required(),
-        isGold:Joi.boolean()
-    });
-    return schema.validate(customer)
-}
 
 module.exports = router;
