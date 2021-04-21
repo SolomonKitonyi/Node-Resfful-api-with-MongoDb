e11a871f43b19cafaba263b57c3374f3c1b121c4
SolomonKitonyi <kitonyisolomon@gmail.com>
2021-04-21 14:33:52 +0300

    add auth route

--- index.js
+++ index.js
@@ -5,11 +5,13 @@ const app = express();
 const courses = require("./routes/courses");
 const customers = require("./routes/customers");
 const users = require("./routes/user");
+const auth = require("./routes/auth");
 
 app.use(express.json());
 app.use("/api/courses", courses);
 app.use("/api/customers", customers);
 app.use("/api/users", users);
+app.use("/api/auth", auth);
 
 mongoose
 	.connect("mongodb://localhost/playground", {
