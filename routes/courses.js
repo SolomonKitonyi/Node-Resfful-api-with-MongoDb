84bc64e868c41f33094bcdeb7739cac34bb63bf0
SolomonKitonyi <kitonyisolomon@gmail.com>
2021-04-22 13:56:43 +0300

    protect routes

--- routes/courses.js
+++ routes/courses.js
@@ -1,53 +1,65 @@
-const {Course,validate} = require('../models/course');
-const express = require('express');
-const mongoose = require('mongoose');
+const auth = require("../middleware/auth");
+const { Course, validate } = require("../models/course");
+const express = require("express");
+const mongoose = require("mongoose");
 const router = express.Router();
-
 
 // router.get('/',(req,res)=>{
 //     res.send('Hello World!!!!!');
 // });
-router.get('/',async(req,res)=>{
-    const courses = await Course.find().sort('name');
-    res.send(courses);
+router.get("/", async (req, res) => {
+	const courses = await Course.find().sort("name");
+	res.send(courses);
 });
 
-
-router.get('/:id',async (req,res)=>{
-    if(!mongoose.Types.ObjectId.isValid(req.params.id))return res.status(400).send("Invalid id")
-    const course = await Course.findById(req.params.id)
-    if(!course) return res.status(404).send("The course you are trying to search was not found");
-    res.send(course);
+router.get("/:id", async (req, res) => {
+	if (!mongoose.Types.ObjectId.isValid(req.params.id))
+		return res.status(400).send("Invalid id");
+	const course = await Course.findById(req.params.id);
+	if (!course)
+		return res
+			.status(404)
+			.send("The course you are trying to search was not found");
+	res.send(course);
 });
 
-
-router.post('/',async(req,res)=>{
-    const {error} = validate(req.body);
-    if(error)return res.status(400).send(error.details[0].message);
-    let course = new Course ({ name: req.body.name});
-    course = await course.save(course);
-    res.send(course);
+router.post("/", auth, async (req, res) => {
+	const { error } = validate(req.body);
+	if (error) return res.status(400).send(error.details[0].message);
+	let course = new Course({ name: req.body.name });
+	course = await course.save(course);
+	res.send(course);
 });
 
+router.put("/:id", async (req, res) => {
+	const { error } = validate(req.body);
+	if (error) return res.status(400).send(error.details[0].message);
+	if (!mongoose.Types.ObjectId.isValid(req.params.id))
+		return res.status(400).send("Invalid course id");
+	const course = await Course.findByIdAndUpdate(
+		req.params.id,
+		{ name: req.body.name },
+		{
+			new: true,
+		}
+	);
+	if (!course)
+		return res
+			.status(404)
+			.send("The course you are trying to search was not found");
 
-router.put('/:id',async(req,res)=>{
-    const {error} = validate(req.body);
-    if(error)return res.status(400).send(error.details[0].message);
-    if(!mongoose.Types.ObjectId.isValid(req.params.id)) return res.status(400).send("Invalid course id");
-    const course = await Course.findByIdAndUpdate(req.params.id,{name:req.body.name},{
-       new:true
-    });
-    if(!course) return res.status(404).send("The course you are trying to search was not found");
-   
-    res.send(course);
+	res.send(course);
 });
 
-router.delete('/:id',async(req,res)=>{
-    if(!mongoose.Types.ObjectId.isValid(req.params.id)) return res.status(400).send("Invalid course id")
-    const course = await Course.findByIdAndRemove(req.params.id);
-    if(!course) return res.status(404).send("The course you are trying to search was not found");
-    res.send(course);
+router.delete("/:id", async (req, res) => {
+	if (!mongoose.Types.ObjectId.isValid(req.params.id))
+		return res.status(400).send("Invalid course id");
+	const course = await Course.findByIdAndRemove(req.params.id);
+	if (!course)
+		return res
+			.status(404)
+			.send("The course you are trying to search was not found");
+	res.send(course);
 });
-
 
 module.exports = router;
