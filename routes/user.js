be93f7bd6e6a1e5989a17cfd8dad3bcf212d2465
SolomonKitonyi <kitonyisolomon@gmail.com>
2021-04-23 13:52:26 +0300

    add express-async-errors

--- routes/user.js
+++ routes/user.js
@@ -1,4 +1,3 @@
-const asyncMiddleWare = require("../middleware/async");
 const auth = require("../middleware/auth");
 const bcrypt = require("bcrypt");
 const _ = require("lodash");
@@ -7,44 +6,37 @@ const express = require("express");
 const mongoose = require("mongoose");
 const router = express.Router();
 
-router.get(
-	"/me",
-	auth,
-	asyncMiddleWare(async (req, res) => {
-		const user = await User.findById(req.user._id).select("-password");
-		res.send(user);
-	})
-);
+router.get("/me", auth, async (req, res) => {
+	const user = await User.findById(req.user._id).select("-password");
+	res.send(user);
+});
 
-router.post(
-	"/",
-	asyncMiddleWare(async (req, res) => {
-		const { error } = validate(req.body);
-		if (error) return res.status(400).send(error.details[0].message);
+router.post("/", async (req, res) => {
+	const { error } = validate(req.body);
+	if (error) return res.status(400).send(error.details[0].message);
 
-		let user = await User.findOne({ email: req.body.email });
-		if (user) return res.status(400).send("User already Registered");
+	let user = await User.findOne({ email: req.body.email });
+	if (user) return res.status(400).send("User already Registered");
 
-		user = new User(_.pick(req.body, ["name", "email", "password"]));
+	user = new User(_.pick(req.body, ["name", "email", "password"]));
 
-		const salt = await bcrypt.genSalt(10);
-		user.password = await bcrypt.hash(user.password, salt);
+	const salt = await bcrypt.genSalt(10);
+	user.password = await bcrypt.hash(user.password, salt);
 
-		await user.save();
+	await user.save();
 
-		const token = user.generateAuthToken();
+	const token = user.generateAuthToken();
 
-		res
-			.header("x-auth-token", token)
-			.send(_.pick(user, ["_id", "name", "email"]));
+	res
+		.header("x-auth-token", token)
+		.send(_.pick(user, ["_id", "name", "email"]));
 
-		//same as
+	//same as
 
-		// res.send({
-		// 	name: user.name,
-		// 	password: user.password,
-		// });
-	})
-);
+	// res.send({
+	// 	name: user.name,
+	// 	password: user.password,
+	// });
+});
 
 module.exports = router;
