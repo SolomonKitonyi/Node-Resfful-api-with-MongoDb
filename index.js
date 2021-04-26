c7e9e863d5e790f214a660aaa19c9982453b6bcd
SolomonKitonyi <kitonyisolomon@gmail.com>
2021-04-26 13:49:05 +0300

    add seperation of concerns

--- index.js
+++ index.js
@@ -1,16 +1,11 @@
-const config = require("config");
-const Joi = require("joi");
 const express = require("express");
 const app = express();
 
 require("./startup/logging");
 require("./startup/routes")(app);
 require("./startup/db")();
-
-if (!config.get("jwtPrivateKey")) {
-	console.error("FATAL ERROR: jwtPrivateKey not defined");
-	process.exit(1);
-}
+require("./startup/config")();
+require("./startup/validation")();
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening to port ${port}....`));
